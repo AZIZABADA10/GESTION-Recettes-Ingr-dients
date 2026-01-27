b3aaf9f0001a8ba0287cd85288187b1e9d9db476
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-27 16:32:15 +0000

    Update Recette model

--- app/Models/Recette.php
+++ app/Models/Recette.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recette extends Model
 {
-    //
+    protected $fillable = [
+        'titre',
+        'description',
+        'image',
+        'user_id',
+        'categorie_id'
+    ];
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class);
+    }
+
+    public function ingredients() 
+    {
+        return $this->belongsToMany(Ingredient::class)->withPivot('quantite');
+    }
+
+    public function Etapes()
+    {
+        return $this->hasMany(EtapePreparation::class);
+    }
+
+    public function Commentaire()
+    {
+        return $this->hasMany(Commentaire::class);
+    }
 }
