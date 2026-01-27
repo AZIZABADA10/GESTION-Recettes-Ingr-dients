35ab2c3ef5a50a65b246b74f1cf605ae025c0230
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-27 16:32:14 +0000

    Update Categorie model

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
-    //
+    protected $fillable = [
+        'titre'
+    ];
+
+    public function recettes()
+    {
+        return $this->hasMany(Recette::class);
+    }
 }
