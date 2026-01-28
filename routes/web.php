e0e9eaf27ac4e9c825eff4e3b75b6c1b3f11058b
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-28 09:45:32 +0000

    Mettre a jour les routes web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\RecetteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::resource('recettes', RecetteController::class);
