74980af3fa1c9290a5e994533f6651e3d808e12d
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-29 17:31:48 +0000

    Modification du contrôleur RecetteController

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorie;
 use App\Models\Recette;
 use Illuminate\Http\Request;
 
@@ -25,7 +26,8 @@ class RecetteController extends Controller
      */
     public function create()
     {
-        
+        $categories =Categorie::all();
+        return view('recettes.create',compact('categories'));
     }
 
     /**
@@ -33,7 +35,22 @@ class RecetteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+        'titre' => 'required|min:4',
+        'description'=>'required',
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+        'user_id'=>'required|exists:users,id',
+        'categorie_id'=>'required|exists:categories,id'
+        ]);
+
+        $recette = Recette::create([
+            'titre' => $request->titre,
+            'description'=>$request->description,
+            'image' => $request->image,
+            'user_id'=>$request->user_id,
+            'categorie_id'=>$request->categorie_id]
+        );
+        return redirect()->route('recettes.index');
     }
 
     /**
@@ -41,7 +58,12 @@ class RecetteController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $recette = Recette::with([
+        'commentaires',
+        'categorie',
+        'ingredients',
+        'etapes'])->findOrFail($id);
+        return view('recettes.show',compact('recette'));
     }
 
     /**
