b9d1d0328919898cc8ef74e9a2d43df3e7ecb8a7
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-29 11:54:44 +0000

    Modifié les routes : correction register/login et middleware guest/auth

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,25 @@ use App\Http\Controllers\RecetteController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
-//     return view('welcome');
+//     return view('recettes.index');
 // });
 
+// Route::resource('recettes', RecetteController::class);
+// Route::get('/register',[AuthController::class,'registerForm']);
+// Route::get('/login',[AuthController::class,'loginForm']);
 
-Route::resource('recettes', RecetteController::class);
-Route::get('/register',[AuthController::class,'registerForm']);
+
+Route::middleware('guest')->group(function(){
+
+    Route::get('/login',[AuthController::class,'loginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register',[AuthController::class, 'registerForm'])->name('register');
+    Route::post('/register',[AuthController::class , 'register']);
+
+});
+
+Route::middleware('auth')->group(function(){
+    Route::resource('recettes', RecetteController::class);
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
