bab594d6a7bf8cedee05a97d9790445d79c868a5
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-30 12:54:47 +0000

    feat(recettes): implémentation complète des méthodes edit, update et destroy

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -92,22 +92,81 @@ class RecetteController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $recette = Recette::with(['ingredients', 'etapes'])->findOrFail($id);
+        $categories = Categorie::all();
+
+        return view('recettes.edit', compact('recette', 'categories'));
     }
 
     /**
      * Update the specified resource in storage.
      */
+    
     public function update(Request $request, string $id)
     {
-        //
+        $recette = Recette::findOrFail($id);
+
+        $request->validate([
+            'titre' => 'required|string|min:5',
+            'categorie_id' => 'required|exists:categories,id',
+            'description' => 'required|string',
+            'ingredients.*titre' => 'required|string',
+            'ingredients.*quantite' => 'required',
+            'etapes.*titre' => 'required',
+            'image' => 'nullable|mimes:jpeg,png,svg,jpg',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $imageName = time().'.'.$request->image->extension();
+            $request->image->move(public_path('images'), $imageName);
+            $recette->image = $imageName;
+        }
+        $recette->update([
+            'titre' => $request->titre,
+            'categorie_id' => $request->categorie_id,
+            'description' => $request->description,
+        ]);
+
+        $recette->ingredients()->detach();
+
+        foreach ($request->ingredients as $value) {
+            $ingredient = Ingredient::firstOrCreate([
+                'titre' => $value['titre']
+            ]);
+
+            $recette->ingredients()->attach($ingredient->id, [
+                'quantite' => $value['quantite']
+            ]);
+        }
+
+        $recette->etapes()->delete();
+
+        foreach ($request->etapes as $index => $value) {
+            $recette->etapes()->create([
+                'titre' => $value['titre'],
+                'ordre' => $index + 1
+            ]);
+        }
+
+        return redirect()
+            ->route('recettes.show', $recette->id)
+            ->with('success', 'Recette mise à jour avec succès ');
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        $recette = Recette::findOrFail($id);
+
+        $recette->ingredients()->detach();
+        $recette->etapes()->delete();
+        $recette->delete();
+
+        return redirect()
+            ->route('recettes.index')
+            ->with('success', 'Recette supprimée avec succès ');
     }
 }
